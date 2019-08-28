1bb14914642440c04718fe378c21918e134bc9fb
Mirko Brandt <lucioreist@yahoo.de>
2019-08-29 00:28:11 +0200

    cors cross origin bug fix try

--- src/main/java/com/OutOfBounds/Pathfinder/PathfinderApplication.java
+++ src/main/java/com/OutOfBounds/Pathfinder/PathfinderApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PathfinderApplication {
@@ -17,4 +19,12 @@ public class PathfinderApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://pathfinder.mt.haw-hamburg.de");
+			}
+		};
+	}
 }
